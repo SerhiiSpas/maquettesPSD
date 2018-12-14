252b62e75245a2d087f31ee4d281fcc63d81b674
SerhiiSpas <serhiispas@gmail.com>
2018-12-14 09:00:56 +0200

    load-Skills

---
+++ Piroll/js/scripts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+
+function loadSkills(time) {
+    var start = 0 ;
+    var startTwo = 0 ;
+    var startThree = 0 ;
+    var progressElement = document.getElementById('load-Skills');
+    var progressElementTwo = document.getElementById('load-Skills-two');
+    var progressElementThree = document.getElementById('load-Skills-three');//елемент прогрес бара
+    var intervalId = setInterval(function () {
+        if (start>=75) {
+            clearInterval(intervalId);
+        }
+        else  {
+            progressElement.value = start;
+        }
+        start++;
+    }, time);
+    var intervalIdTwo = setInterval(function () {
+        if (startTwo>=90) {
+            clearInterval(intervalIdTwo);
+        }
+        else  {
+            progressElementTwo.value = startTwo;
+        }
+        startTwo++;
+    }, time);
+    var intervalIdThree = setInterval(function () {
+        if (startThree>=65) {
+            clearInterval(intervalIdThree);
+        }
+        else  {
+            progressElementThree.value = startThree;
+        }
+        startThree++;
+    }, time);
+}
+
+loadSkills(200);
+
