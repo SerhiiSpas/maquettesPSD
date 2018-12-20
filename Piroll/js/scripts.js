ffbb7b611f8e90b1d1375dbb94f8e1db71d0c0de
SerhiiSpas <serhiispas@gmail.com>
2018-12-20 14:22:10 +0200

    scroll

--- Piroll/js/scripts.js
+++ Piroll/js/scripts.js
@@ -40,5 +40,13 @@ function loadSkills(time) {
 
 loadSkills(200);
 
-
-
+$(document).ready(function () {
+    $("a[href*=#]").on("click", function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1000);
+        e.preventDefault();
+        return false;
+    });
+});
