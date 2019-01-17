d6da4a1f9accbd907f8f5dddbd766c656c9e15f4
SerhiiSpas <serhiispas@gmail.com>
2019-01-17 09:25:08 +0200

    TITLE

--- TimeLine/TimeLinerCircle.js
+++ TimeLine/TimeLinerCircle.js
@@ -16,34 +16,3 @@ function move(e) {
     removeActiveElem.classList = "";
     createActiveElem.classList = "active";
 }
-
-
-/*
-buttons.forEach((button, i) => {
-    button.style.top = Math.sin(-Math.PI / 2 + i * increase) * radius + "px";
-    button.style.left = Math.cos(-Math.PI / 2 + i * increase) * radius + "px";
-    button.addEventListener("click", move);
-});
-
-function move(e) {
-    const n = buttons.indexOf(e.target);
-    const endAngle = (n % count) * increase;
-    (function turn() {
-        if (Math.abs(endAngle - angle) > 1 / 8) {
-            const sign = endAngle > angle ? 1 : -1;
-            angle = angle + sign / 8;
-            setTimeout(turn, 40);
-        } else {
-            angle = endAngle;
-        }
-        buttons.forEach((button, i) => {
-            button.style.top =
-                Math.sin(-Math.PI / 2 + i * increase - angle) * radius + "px";
-            button.style.left =
-                Math.cos(-Math.PI / 2 + i * increase - angle) * radius + "px";
-        });
-    })();
-}*/
-
-
-
