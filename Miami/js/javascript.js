d95e9a6430d48088e9b08d479c4154b0e415fefa
SerhiiSpas <serhiispas@gmail.com>
2019-01-29 08:29:07 +0200

    slide

--- Miami/js/javascript.js
+++ Miami/js/javascript.js
@@ -1,12 +1,25 @@
-$(function () {
-    'use strict';
 
-    $('a[href^="#"]').click(function () {
-    let target = $(this).attr('href');
-    $('html, body').animate(
-        {
-            scrollTop:$(target).offsetTop().top
-        }, 800
-    );
-    });
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+    return false;
+});
+
+
+var offset = $('#bottom-header').offset();
+$(window).scroll( function () {
+    if( $(window).scrollTop() > offset.top){
+        $('#bottom-header').addClass('fixedNav');
+    } else {
+        $('#bottom-header').removeClass('fixedNav')
+    }
+});
+
+$('#slide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoPlay: true,
+    autoplaySpeed: 2000,
 });
