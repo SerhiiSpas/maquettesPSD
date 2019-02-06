f2d7371bfbdeeff933947ad368b91d7751e63c09
SerhiiSpas <serhiispas@gmail.com>
2019-02-06 10:00:22 +0200

    about

--- Kappe/js/javascript.js
+++ Kappe/js/javascript.js
@@ -3,5 +3,10 @@ $(function() {
 
     $("#info").click(function() {
         $('#info_box').toggleClass('open');
-    })
+    });
+
+    $('.pr_bar').each(function () {
+        let width = $(this).data('skill');
+        $(this).css('width', width + '%')
+    });
 });
